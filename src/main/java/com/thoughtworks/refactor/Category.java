79bf5fa807aaa668b8fae6d8efb5eed5e53c2901
CHENMA18 <chenma18@oocl.com>
2021-06-30 14:34:08 +0800

    [refactor] introduce parameter object Category

---
+++ src/main/java/com/thoughtworks/refactor/Category.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.refactor;
+
+public class Category {
+    private final String strType;
+
+    public Category(String strType) {
+        this.strType = strType;
+    }
+
+    public String getStrType() {
+        return strType;
+    }
+}
