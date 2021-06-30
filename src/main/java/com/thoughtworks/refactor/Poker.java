5cd9a9f84f96c7f10f0ba6ce354897922f8110e5
CHENMA18 <chenma18@oocl.com>
2021-06-30 15:58:40 +0800

    [refactor] extract compareHighCard

--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -33,19 +33,25 @@ public class Poker {
             } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 7) { // One Pair
                 winResult = compareOnePair(blackHandsObj, whiteHandsObj);
             } else { // High Card
-                for (int i = 0; i < 5; i++) {
-                    if (blackHandsObj.getDescendingHandsNumbers()[i] < whiteHandsObj.getDescendingHandsNumbers()[i]) {
-                        String sig = intNumber(whiteHandsObj.getDescendingHandsNumbers()[i]);
-                        winResult = "white wins - high card:" + sig;
-                        break;
-                    } else if (blackHandsObj.getDescendingHandsNumbers()[i] > whiteHandsObj.getDescendingHandsNumbers()[i]) {
-                        String sig = intNumber(blackHandsObj.getDescendingHandsNumbers()[i]);
-                        winResult = "black wins - high card:" + sig;
-                        break;
-                    } else {
-                        winResult = "tie";
-                    }
-                }
+                winResult = compareHighCard(blackHandsObj, whiteHandsObj);
+            }
+        }
+        return winResult;
+    }
+
+    private String compareHighCard( Hands blackHandsObj, Hands whiteHandsObj) {
+        String winResult="";
+        for (int i = 0; i < 5; i++) {
+            if (blackHandsObj.getDescendingHandsNumbers()[i] < whiteHandsObj.getDescendingHandsNumbers()[i]) {
+                String sig = intNumber(whiteHandsObj.getDescendingHandsNumbers()[i]);
+                winResult = "white wins - high card:" + sig;
+                break;
+            } else if (blackHandsObj.getDescendingHandsNumbers()[i] > whiteHandsObj.getDescendingHandsNumbers()[i]) {
+                String sig = intNumber(blackHandsObj.getDescendingHandsNumbers()[i]);
+                winResult = "black wins - high card:" + sig;
+                break;
+            } else {
+                winResult = "tie";
             }
         }
         return winResult;
