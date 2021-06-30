9b2f293a4c7f8b9770d224c7ad329080d6f29f16
CHENMA18 <chenma18@oocl.com>
2021-06-30 15:46:36 +0800

    [refactor] inline

--- src/main/java/com/thoughtworks/refactor/Category.java
+++ src/main/java/com/thoughtworks/refactor/Category.java
@@ -1,21 +1,21 @@
 package com.thoughtworks.refactor;
 
 public class Category {
-    private final String strType;
+    private final String category;
 
     public Category(String strType) {
-        this.strType = strType;
+        category = strType;
     }
 
-    public String getStrType() {
-        return strType;
+    public String getCategory() {
+        return category;
     }
 
-    int getRanking() {
+    int judgeHandsCategoryRanking() {
         int index = -1;
         String[] type = {"StraightFlush", "FourOfAKind", "FullHouse", "Flush", "Straight", "ThreeOfAKind", "TwoPair", "OnePair", "HighCard"};
         for (int i = 0; i < 9; i++) {
-            if (type[i].equals(getStrType())) {
+            if (type[i].equals(getCategory())) {
                 index = i;
             }
         }
--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -12,36 +12,12 @@ public class Poker {
 
         final Hands whiteHandsObj = new Hands(whiteHands);
 
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i1 = 0; i1 < whiteHandsObj.getDescendingHandsNumbers().length; i1++) {
-            if (map.get(whiteHandsObj.getDescendingHandsNumbers()[i1]) != null) {
-                map.put(whiteHandsObj.getDescendingHandsNumbers()[i1], map.get(whiteHandsObj.getDescendingHandsNumbers()[i1]) + 1);
-            } else {
-                map.put(whiteHandsObj.getDescendingHandsNumbers()[i1], 1);
-            }
-        }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>();
-        list.addAll(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> arg0, Map.Entry<Integer, Integer> arg1) {
-                return arg1.getValue().compareTo(arg0.getValue());
-            }
-        });
-        int[] arrayresult = new int[list.size()];
-        int i1 = 0;
-        for (Map.Entry<Integer, Integer> entry : list) {
-            arrayresult[i1] = entry.getKey();
-            i1++;
-        }
-        int[] whiteDistinctDescendingHandsNumbers = arrayresult;
-        int[] whiteNoRepeatNumbers = PokerUtil.getDescendingNoRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers());
-
-        if (blackHandsObj.getCategory().getRanking() < whiteHandsObj.getCategory().getRanking()) {
-            winResult = "black wins - " + CATEGORIES[blackHandsObj.getCategory().getRanking()];
-        } else if (blackHandsObj.getCategory().getRanking() > whiteHandsObj.getCategory().getRanking()) {
-            winResult = "white wins - " + CATEGORIES[whiteHandsObj.getCategory().getRanking()];
+        if (blackHandsObj.getCategory().judgeHandsCategoryRanking() < whiteHandsObj.getCategory().judgeHandsCategoryRanking()) {
+            winResult = "black wins - " + CATEGORIES[blackHandsObj.getCategory().judgeHandsCategoryRanking()];
+        } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() > whiteHandsObj.getCategory().judgeHandsCategoryRanking()) {
+            winResult = "white wins - " + CATEGORIES[whiteHandsObj.getCategory().judgeHandsCategoryRanking()];
         } else {
-            if (blackHandsObj.getCategory().getRanking() == 0) { // Straight Flush
+            if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 0) { // Straight Flush
                 if (blackHandsObj.getDescendingHandsNumbers()[0] < whiteHandsObj.getDescendingHandsNumbers()[0]) {
                     String sig = intNumber(whiteHandsObj.getDescendingHandsNumbers()[0]);
                     winResult = "white wins - high card:" + sig;
@@ -51,23 +27,23 @@ public class Poker {
                 } else {
                     winResult = "tie";
                 }
-            } else if (blackHandsObj.getCategory().getRanking() == 1) { // Four Of A Kind
-                if (blackHandsObj.getDistinctDescendingHandsNumbers()[0] < whiteDistinctDescendingHandsNumbers[0]) {
-                    String sig = intNumber(whiteDistinctDescendingHandsNumbers[0]);
+            } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 1) { // Four Of A Kind
+                if (getDistinctDescendingHandsNumbers(blackHandsObj.getDescendingHandsNumbers())[0] < getDistinctDescendingHandsNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]) {
+                    String sig = intNumber(getDistinctDescendingHandsNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "white wins - high card:" + sig;
                 } else {
-                    String sig = intNumber(blackHandsObj.getDistinctDescendingHandsNumbers()[0]);
+                    String sig = intNumber(getDistinctDescendingHandsNumbers(blackHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "black wins - high card:" + sig;
                 }
-            } else if (blackHandsObj.getCategory().getRanking() == 2) { // Full House
-                if (blackHandsObj.getDistinctDescendingHandsNumbers()[0] < whiteDistinctDescendingHandsNumbers[0]) {
-                    String sig = intNumber(whiteDistinctDescendingHandsNumbers[0]);
+            } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 2) { // Full House
+                if (getDistinctDescendingHandsNumbers(blackHandsObj.getDescendingHandsNumbers())[0] < getDistinctDescendingHandsNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]) {
+                    String sig = intNumber(getDistinctDescendingHandsNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "white wins - high card:" + sig;
                 } else {
-                    String sig = intNumber(blackHandsObj.getDistinctDescendingHandsNumbers()[0]);
+                    String sig = intNumber(getDistinctDescendingHandsNumbers(blackHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "black wins - high card:" + sig;
                 }
-            } else if (blackHandsObj.getCategory().getRanking() == 3) { // Flush
+            } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 3) { // Flush
                 for (int i = 0; i < 5; i++) {
                     if (blackHandsObj.getDescendingHandsNumbers()[i] < whiteHandsObj.getDescendingHandsNumbers()[i]) {
                         String sig = intNumber(whiteHandsObj.getDescendingHandsNumbers()[i]);
@@ -81,7 +57,7 @@ public class Poker {
                         winResult = "tie";
                     }
                 }
-            } else if (blackHandsObj.getCategory().getRanking() == 4) { // Straight
+            } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 4) { // Straight
                 if (blackHandsObj.getDescendingHandsNumbers()[0] < whiteHandsObj.getDescendingHandsNumbers()[0]) {
                     String sig = intNumber(whiteHandsObj.getDescendingHandsNumbers()[0]);
                     winResult = "white wins - high card:" + sig;
@@ -91,52 +67,52 @@ public class Poker {
                 } else {
                     winResult = "tie";
                 }
-            } else if (blackHandsObj.getCategory().getRanking() == 5) { // Three Of A Kind
-                if (blackHandsObj.getDescendingRepeatNumbers()[0] < whiteHandsObj.getDescendingRepeatNumbers()[0]) {
-                    String sig = intNumber(whiteHandsObj.getDescendingRepeatNumbers()[0]);
+            } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 5) { // Three Of A Kind
+                if (getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0] < getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]) {
+                    String sig = intNumber(getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "white wins - high card:" + sig;
                 } else {
-                    String sig = intNumber(blackHandsObj.getDescendingRepeatNumbers()[0]);
+                    String sig = intNumber(getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "black wins - high card:" + sig;
                 }
-            } else if (blackHandsObj.getCategory().getRanking() == 6) { // Two Pair
+            } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 6) { // Two Pair
                 for (int i = 0; i < 2; i++) {
-                    if (blackHandsObj.getDescendingRepeatNumbers()[i] < whiteHandsObj.getDescendingRepeatNumbers()[i]) {
-                        String sig = intNumber(whiteHandsObj.getDescendingRepeatNumbers()[i]);
+                    if (getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[i] < getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[i]) {
+                        String sig = intNumber(getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[i]);
                         winResult = "white wins - high card:" + sig;
                         break;
-                    } else if (blackHandsObj.getDescendingRepeatNumbers()[i] > whiteHandsObj.getDescendingRepeatNumbers()[i]) {
-                        String sig = intNumber(blackHandsObj.getDescendingRepeatNumbers()[i]);
+                    } else if (getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[i] > getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[i]) {
+                        String sig = intNumber(getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[i]);
                         winResult = "black wins - high card:" + sig;
                         break;
                     }
                 }
                 if (winResult == "") {
-                    if (blackHandsObj.getDescendingNoRepeatNumbers()[0] < whiteNoRepeatNumbers[0]) {
-                        String sig = intNumber(whiteNoRepeatNumbers[0]);
+                    if (getDescendingNoRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0] < getDescendingNoRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]) {
+                        String sig = intNumber(getDescendingNoRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]);
                         winResult = "white wins - high card:" + sig;
-                    } else if (blackHandsObj.getDescendingNoRepeatNumbers()[0] > whiteNoRepeatNumbers[0]) {
-                        String sig = intNumber(blackHandsObj.getDescendingNoRepeatNumbers()[0]);
+                    } else if (getDescendingNoRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0] > getDescendingNoRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]) {
+                        String sig = intNumber(getDescendingNoRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0]);
                         winResult = "black wins - high card:" + sig;
                     } else {
                         winResult = "tie";
                     }
                 }
-            } else if (blackHandsObj.getCategory().getRanking() == 7) { // One Pair
-                if (blackHandsObj.getDescendingRepeatNumbers()[0] < whiteHandsObj.getDescendingRepeatNumbers()[0]) {
-                    String sig = intNumber(whiteHandsObj.getDescendingRepeatNumbers()[0]);
+            } else if (blackHandsObj.getCategory().judgeHandsCategoryRanking() == 7) { // One Pair
+                if (getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0] < getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]) {
+                    String sig = intNumber(getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "white wins - high card:" + sig;
-                } else if (blackHandsObj.getDescendingRepeatNumbers()[0] > whiteHandsObj.getDescendingRepeatNumbers()[0]) {
-                    String sig = intNumber(blackHandsObj.getDescendingRepeatNumbers()[0]);
+                } else if (getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0] > getDescendingRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[0]) {
+                    String sig = intNumber(getDescendingRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[0]);
                     winResult = "black wins - high card:" + sig;
                 } else {
                     for (int i = 0; i < 3; i++) {
-                        if (blackHandsObj.getDescendingNoRepeatNumbers()[i] < whiteNoRepeatNumbers[i]) {
-                            String sig = intNumber(whiteNoRepeatNumbers[i]);
+                        if (getDescendingNoRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[i] < getDescendingNoRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[i]) {
+                            String sig = intNumber(getDescendingNoRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[i]);
                             winResult = "white wins - high card:" + sig;
                             break;
-                        } else if (blackHandsObj.getDescendingNoRepeatNumbers()[i] > whiteNoRepeatNumbers[i]) {
-                            String sig = intNumber(blackHandsObj.getDescendingNoRepeatNumbers()[i]);
+                        } else if (getDescendingNoRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[i] > getDescendingNoRepeatNumbers(whiteHandsObj.getDescendingHandsNumbers())[i]) {
+                            String sig = intNumber(getDescendingNoRepeatNumbers(blackHandsObj.getDescendingHandsNumbers())[i]);
                             winResult = "black wins - high card:" + sig;
                             break;
                         } else {
@@ -163,9 +139,103 @@ public class Poker {
         return winResult;
     }
 
+    private int[] getDescendingNoRepeatNumbers(int[] blackDescendingHandsNumbers) {
+        return noOrRepeatNumber(blackDescendingHandsNumbers, 1);
+    }
+
+    private int[] getDescendingRepeatNumbers(int[] blackDescendingHandsNumbers) {
+        return noOrRepeatNumber(blackDescendingHandsNumbers, 0);
+    }
+
     private String intNumber(int i) {
         String[] strNumber = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
         return strNumber[i - 2];
+    }
+
+    private int[] getDistinctDescendingHandsNumbers(int[] number) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < number.length; i++) {
+            if (map.get(number[i]) != null) {
+                map.put(number[i], map.get(number[i]) + 1);
+            } else {
+                map.put(number[i], 1);
+            }
+        }
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>();
+        list.addAll(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            public int compare(Map.Entry<Integer, Integer> arg0, Map.Entry<Integer, Integer> arg1) {
+                return arg1.getValue().compareTo(arg0.getValue());
+            }
+        });
+        int[] arrayresult = new int[list.size()];
+        int i = 0;
+        for (Map.Entry<Integer, Integer> entry : list) {
+            arrayresult[i] = entry.getKey();
+            i++;
+        }
+        return arrayresult;
+    }
+
+    // First get the number of occurrences of each element in the array,
+    // then calculate the number of occurrences greater than 1 and the number of occurrences equal to 1.
+    private int[] noOrRepeatNumber(int[] number, int flag) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < number.length; i++) {
+            if (map.get(number[i]) != null) {
+                map.put(number[i], map.get(number[i]) + 1);
+            } else {
+                map.put(number[i], 1);
+            }
+        }
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>();
+        list.addAll(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            public int compare(Map.Entry<Integer, Integer> arg0, Map.Entry<Integer, Integer> arg1) {
+                return arg1.getValue().compareTo(arg0.getValue());
+            }
+        });
+        int[] repeatnumber = new int[list.size()];
+        int[] norepeatnumber = new int[list.size()];
+        int i = 0;
+        if (flag == 0) {
+            for (Map.Entry<Integer, Integer> entry : list) {
+                if (entry.getValue() > 1) {
+                    repeatnumber[i] = entry.getKey();
+                    i++;
+                }
+            }
+        } else {
+            for (Map.Entry<Integer, Integer> entry : list) {
+                if (entry.getValue() == 1) {
+                    norepeatnumber[i] = entry.getKey();
+                    i++;
+                }
+            }
+        }
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        if (flag == 0) {
+            for (i = 0; i < repeatnumber.length; i++) {
+                hashSet.add(repeatnumber[i]);
+            }
+        } else {
+            for (i = 0; i < norepeatnumber.length; i++) {
+                hashSet.add(norepeatnumber[i]);
+            }
+        }
+        hashSet.remove(0);
+        int[] result = new int[hashSet.size()];
+        i = 0;
+        Iterator<Integer> iterator = hashSet.iterator();
+        while (iterator.hasNext()) {
+            result[i] = iterator.next();
+            i++;
+        }
+        int[] reResult = new int[result.length];
+        for (i = 0; i < result.length; i++) {
+            reResult[i] = result[result.length - i - 1];
+        }
+        return reResult;
     }
 
 }
